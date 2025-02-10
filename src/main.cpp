0864688ef78cad2449a15d7b5f625c445d012a0a
Rain <51992995+rainwl@users.noreply.github.com>
2025-02-10 17:50:35 +0800

    enh : support multiple models

--- src/main.cpp
+++ src/main.cpp
@@ -1,163 +1,154 @@
-#pragma clang diagnostic push
-#pragma ide diagnostic ignored "cppcoreguidelines-narrowing-conversions"
-#include "igl/read_triangle_mesh.h"
-#include "igl/opengl/glfw/Viewer.h"
-#include "igl/opengl/glfw/imgui/ImGuiPlugin.h"
-#include "igl/opengl/glfw/imgui/ImGuiMenu.h"
-#include "igl/opengl/glfw/imgui/ImGuizmoWidget.h"
-#include "GLFW/glfw3.h"
-#include "unproject_onto_mesh.h"
+#include <igl/read_triangle_mesh.h>
+#include <igl/opengl/glfw/Viewer.h>
+#include <igl/opengl/glfw/imgui/ImGuiPlugin.h>
+#include <igl/opengl/glfw/imgui/ImGuizmoWidget.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+#include <igl/unproject_onto_mesh.h>
+#include "dmSimSvrUnity.pb.h"
+#include "ecal/ecal.h"
+#include "ecal/msg/protobuf/publisher.h"
+#include "google/protobuf/any.pb.h"
+
+std::string deformable_path = "./acl.off";
+std::string bone_path       = "./bone.off";
+std::string hook_path       = "./hook.off";
+
+void eCALSend(Eigen::Vector3d translation, Eigen::Quaterniond quaternion, eCAL::CPublisher &pub, const int instrumentId) {
+  auto *imSend = new dm::InstrumentMsg;
+  imSend->set_insid(instrumentId);
+  imSend->set_visible(true);
+  auto *posSend = new dm::Vec3;
+  posSend->set_x(static_cast<float>(translation.x()));
+  posSend->set_y(static_cast<float>(translation.y()));
+  posSend->set_z(static_cast<float>(translation.z()));
+  auto *rotSend = new dm::Vec4;
+  rotSend->set_x(static_cast<float>(quaternion.x()));
+  rotSend->set_y(static_cast<float>(quaternion.y()));
+  rotSend->set_z(static_cast<float>(quaternion.z()));
+  rotSend->set_w(static_cast<float>(quaternion.w()));
+
+  imSend->set_allocated_pos(posSend);
+  imSend->set_allocated_rot(rotSend);
+
+  google::protobuf::Any any;
+  any.PackFrom(*imSend);
+  std::size_t sendLen  = any.ByteSizeLong();
+  auto *      sendData = new std::uint8_t[sendLen]{0};
+  any.SerializePartialToArray((void *) (sendData), static_cast<int>(sendLen));
+  pub.SetID(6);
+  if (const std::size_t sendCode = pub.Send(sendData, sendLen); sendCode != sendLen) { std::cout << pub.GetTopicName() << " send failed\n"; }
+  delete[] sendData;
+  delete imSend;
+}
 
 int main(int argc, char *argv[]) {
-#pragma region Load Mesh
-  // Load a mesh from file
-  Eigen::MatrixXd V, C;
-  Eigen::MatrixXi F;
-  igl::read_triangle_mesh("./couplingdown.off", V, F);
-  C = Eigen::MatrixXd::Constant(F.rows(), 3, 1);
-#pragma endregion
+  eCAL::Initialize(1, nullptr, "GeometryKit");
+  eCAL::CPublisher pub_navi("simsvr-data");
 
-#pragma region Viewer
-  // Set up viewer
-  igl::opengl::glfw::Viewer vr;
-  vr.callback_mouse_down =
-      [&V, &F, &C](igl::opengl::glfw::Viewer &viewer, int, int) -> bool {
-        int fid;
-        Eigen::Vector3f bc;
-        // Cast a ray in the view direction starting from the mouse position
-        double x = viewer.current_mouse_x;
-        double y = viewer.core().viewport(3) - viewer.current_mouse_y;
-        if (igl::unproject_onto_mesh(Eigen::Vector2f(x, y),
-                                     viewer.core().view,
-                                     viewer.core().proj,
-                                     viewer.core().viewport,
-                                     V,
-                                     F,
-                                     fid,
-                                     bc)) {
-          // paint hit red
-          C.row(fid) << 1, 0, 0;
+  Eigen::MatrixXd V1, V2;
+  Eigen::MatrixXi F1, F2;
 
-          std::cout << "Face index: " << fid << std::endl;
+  if (!igl::read_triangle_mesh(bone_path, V1, F1)) { return EXIT_FAILURE; }
+  if (!igl::read_triangle_mesh(hook_path, V2, F2)) { return EXIT_FAILURE; }
 
-          // Retrieve the indices of the vertices that make up the face
-          int v1 = F(fid, 0);
-          int v2 = F(fid, 1);
-          int v3 = F(fid, 2);
+  igl::opengl::glfw::Viewer viewer;
 
-          // Output the coordinates of the vertices
-          Eigen::Vector3d coord1 = V.row(v1);
-          Eigen::Vector3d coord2 = V.row(v2);
-          Eigen::Vector3d coord3 = V.row(v3);
+  viewer.append_mesh();
+  viewer.data(0).set_mesh(V1, F1);
+  viewer.data(0).set_colors(Eigen::RowVector3d(1, 1, 1));
+  viewer.data(0).show_lines = false;
 
-          // Check if x, y, or z coordinates are the same among the vertices
-          if (coord1.x() == coord2.x() && coord2.x() == coord3.x()) {
-            std::cout << "All vertices of the selected face have the same x value: " << coord1.x() << std::endl;
-            double X = coord1.x();
+  viewer.append_mesh();
+  viewer.data(1).set_mesh(V2, F2);
+  viewer.data(1).set_colors(Eigen::RowVector3d(1, 1, 1));
+  viewer.data(1).show_lines = false;
 
-            // Traverse all faces to find and color faces where all vertices have the same y coordinate equal to X
-            for (int i = 0; i < F.rows(); ++i) {
-              // Get vertex indices of the face
-              int v1 = F(i, 0);
-              int v2 = F(i, 1);
-              int v3 = F(i, 2);
-
-              // Check if all vertices of this face have the y coordinate equal to X
-              if (V(v1, 0) == X && V(v2, 0) == X && V(v3, 0) == X) {
-                // Set the color for the corresponding face
-                std::cout << i << std::endl;
-                C.row(i) << 1, 0, 0;
-              }
-            }
-          } else if (coord1.y() == coord2.y() && coord2.y() == coord3.y()) {
-            std::cout << "All vertices of the selected face have the same y value: " << coord1.y() << std::endl;
-            double commonY = coord1.y();
-
-            // Traverse all faces to find and color faces where all vertices have the same y coordinate equal to commonY
-            for (int i = 0; i < F.rows(); ++i) {
-              // Get vertex indices of the face
-              int v1 = F(i, 0);
-              int v2 = F(i, 1);
-              int v3 = F(i, 2);
-
-              // Check if all vertices of this face have the y coordinate equal to commonY
-              if (V(v1, 1) == commonY && V(v2, 1) == commonY && V(v3, 1) == commonY) {
-                // Set the color for the corresponding face
-                std::cout << i << std::endl;
-                C.row(i) << 1, 0, 0;
-              }
-            }
-          } else if (coord1.z() == coord2.z() && coord2.z() == coord3.z()) {
-            std::cout << "All vertices of the selected face have the same z value: " << coord1.z() << std::endl;
-            double commonZ = coord1.z();
-
-            // Traverse all faces to find and color faces where all vertices have the same y coordinate equal to commonZ
-            for (int i = 0; i < F.rows(); ++i) {
-              // Get vertex indices of the face
-              int v1 = F(i, 0);
-              int v2 = F(i, 1);
-              int v3 = F(i, 2);
-
-              // Check if all vertices of this face have the y coordinate equal to commonZ
-              if (V(v1, 2) == commonZ && V(v2, 2) == commonZ && V(v3, 2) == commonZ) {
-                // Set the color for the corresponding face
-                std::cout << i << std::endl;
-                C.row(i) << 1, 0, 0;
-              }
-            }
-          }
-          viewer.data().set_colors(C);
-          return true;
-        }
-        return false;
-      };
-  vr.data().set_mesh(V, F);
-  vr.data().set_colors(C);
-  vr.data().show_lines = false;
-#pragma endregion
-
-#pragma region imgui
   igl::opengl::glfw::imgui::ImGuiPlugin imgui_plugin;
-  vr.plugins.push_back(&imgui_plugin);
+  viewer.plugins.push_back(&imgui_plugin);
 
-  // Add a 3D gizmo plugin
-  igl::opengl::glfw::imgui::ImGuizmoWidget gizmo;
-  imgui_plugin.widgets.push_back(&gizmo);
-  // Initialize ImGuizmo at mesh centroid
-  gizmo.T.block(0, 3, 3, 1) =
-      0.5 * (V.colwise().maxCoeff() + V.colwise().minCoeff()).transpose().cast<float>();
-  // Update can be applied relative to this remembered initial transform
-  const Eigen::Matrix4f T0 = gizmo.T;
-  // Attach callback to apply imguizmo's transform to mesh
-  gizmo.callback = [&](const Eigen::Matrix4f &T) {
-    const Eigen::Matrix4d TT = (T * T0.inverse()).cast<double>().transpose();
-    vr.data().set_vertices(
-        (V.rowwise().homogeneous() * TT).rowwise().hnormalized());
-    vr.data().compute_normals();
+  igl::opengl::glfw::imgui::ImGuizmoWidget gizmo1, gizmo2;
+  imgui_plugin.widgets.push_back(&gizmo1);
+  imgui_plugin.widgets.push_back(&gizmo2);
+
+  gizmo1.visible = false;
+  gizmo2.visible = false;
+
+  gizmo1.T.block(0, 3, 3, 1) = 0.5 * (V1.colwise().maxCoeff() + V1.colwise().minCoeff()).transpose().cast<float>();
+  gizmo2.T.block(0, 3, 3, 1) = 0.5 * (V2.colwise().maxCoeff() + V2.colwise().minCoeff()).transpose().cast<float>();
+
+  Eigen::Matrix4f T0_1 = gizmo1.T;
+  Eigen::Matrix4f T0_2 = gizmo2.T;
+
+  gizmo1.callback = [&](const Eigen::Matrix4f &T) {
+    const Eigen::Matrix4d TT = (T * T0_1.inverse()).cast<double>().transpose();
+    viewer.data(0).set_vertices((V1.rowwise().homogeneous() * TT).rowwise().hnormalized());
+    viewer.data(0).compute_normals();
+
+    Eigen::Matrix4d          M              = (T * T0_1.inverse()).cast<double>();
+    const Eigen::Vector3d    translation    = M.block<3, 1>(0, 3);
+    const Eigen::Matrix3d    rotationMatrix = M.block<3, 3>(0, 0);
+    const Eigen::Quaterniond quaternion(rotationMatrix);
+    eCALSend(translation, quaternion, pub_navi, 302);
   };
-  // Maya-style keyboard shortcuts for operation
-  vr.callback_key_pressed = [&](decltype(vr) &, unsigned int key, int mod) {
-    switch (key) {
-      case ' ': gizmo.visible = !gizmo.visible;
+  gizmo2.callback = [&](const Eigen::Matrix4f &T) {
+    const Eigen::Matrix4d TT = (T * T0_2.inverse()).cast<double>().transpose();
+    viewer.data(1).set_vertices((V2.rowwise().homogeneous() * TT).rowwise().hnormalized());
+    viewer.data(1).compute_normals();
+
+    Eigen::Matrix4d          M              = (T * T0_2.inverse()).cast<double>();
+    const Eigen::Vector3d    translation    = M.block<3, 1>(0, 3);
+    const Eigen::Matrix3d    rotationMatrix = M.block<3, 3>(0, 0);
+    const Eigen::Quaterniond quaternion(rotationMatrix);
+    eCALSend(translation, quaternion, pub_navi, 303);
+  };
+  viewer.callback_mouse_down = [&](igl::opengl::glfw::Viewer &v, const int button, int modifier) {
+    if (button == GLFW_MOUSE_BUTTON_LEFT) {
+      gizmo1.visible = false;
+      gizmo2.visible = false;
+
+      const double          x = viewer.current_mouse_x;
+      const double          y = viewer.core().viewport(3) - static_cast<double>(viewer.current_mouse_y);
+      const Eigen::Vector2f mouse_pos(x, y);
+
+      int selected_mesh_index = -1;
+      for (int i = 0; i < v.data_list.size(); ++i) {
+        Eigen::Vector3f bc;
+        if (int fid; igl::unproject_onto_mesh(mouse_pos, v.core().view, v.core().proj, v.core().viewport,
+                                              v.data_list[i].V, v.data_list[i].F, fid, bc)) {
+          selected_mesh_index = i;
+          break;
+        }
+      }
+      if (selected_mesh_index != -1) {
+        gizmo1.visible = (selected_mesh_index == 0);
+        gizmo2.visible = (selected_mesh_index == 1);
         return true;
+      }
+    }
+    return false;
+  };
+  viewer.callback_key_pressed = [&](decltype(viewer) &, const unsigned int key, int mod) {
+    switch (key) {
       case 'W':
-      case 'w': gizmo.operation = ImGuizmo::TRANSLATE;
+      case 'w': gizmo1.operation = ImGuizmo::TRANSLATE;
+        gizmo2.operation = ImGuizmo::TRANSLATE;
         return true;
       case 'E':
-      case 'e': gizmo.operation = ImGuizmo::ROTATE;
+      case 'e': gizmo1.operation = ImGuizmo::ROTATE;
+        gizmo2.operation = ImGuizmo::ROTATE;
         return true;
       case 'R':
-      case 'r': gizmo.operation = ImGuizmo::SCALE;
+      case 'r': gizmo1.operation = ImGuizmo::SCALE;
+        gizmo2.operation = ImGuizmo::SCALE;
         return true;
-      default:break;
+      default: ;
     }
     return false;
   };
 
-  igl::opengl::glfw::imgui::ImGuiMenu menu;
-  imgui_plugin.widgets.push_back(&menu);
-  vr.launch();
-#pragma endregion
+  // igl::opengl::glfw::imgui::ImGuiMenu menu;
+  // imgui_plugin.widgets.push_back(&menu);
+  viewer.launch();
+  return EXIT_SUCCESS;
 }
-
-#pragma clang diagnostic pop
